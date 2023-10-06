6129f84b20c2fa003f4fbeea5ab1d32b00700ab8
pradipbkamble <pradipbkamble001@gmail.com>
2023-10-07 00:14:23 +0530

    work on html and css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,18 @@
-const cl=console.log;
+let cl =console.log;
+
+let bgcolor=document.getElementById("bgcolor");
+let alld=[...document.querySelectorAll(".all")]
 
 
-let CreateClock =()=>{
-   const Digitalclock= document.getElementById("Digitalclock")
-    let d= new Date()
-        let hr= d.getHours();
-        let min= d.getMinutes();
-let sec= d.getSeconds();
-let session= "AM";
+let galaryfilter=(eve)=>{
+  cl(eve.target.value)
+let classname= eve.target.value;
+alld.forEach(diva =>
+  diva.classList.add(`d-none`))
 
-session = (hr >= 12)? "PM" : "AM";
+  const selectdiv=[...document.getElementsByClassName(classname)]
+selectdiv.forEach(ev1=>
+  ev1.classList.remove(`d-none`))
 
-if (hr>12){
-     hr = hr - 12
 }
-sec= (sec < 10) ?"0" + sec : sec;
-min= (min < 10) ?"0" + min : min;
-hr= (hr < 10) ?"0" + hr : hr;
-
-//if(sec < 10){
-  //  sec = "0" + sec
-//}
-let result = `${hr}: ${min}: ${sec} ${session}`;
-Digitalclock.innerHTML = result;
-setTimeout(CreateClock, 1000);
-}
-CreateClock();
+bgcolor.addEventListener("change",galaryfilter);
